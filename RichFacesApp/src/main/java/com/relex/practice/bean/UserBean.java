2fe56ec95f1f17c563b33365f1457db893550c93
alexeyk <kunak94@mail.ru>
2014-07-17 13:39:11 +0400

    CONNECTON!!! ADD USERS!!!

--- RichFacesApp/src/main/java/com/relex/practice/bean/UserBean.java
+++ RichFacesApp/src/main/java/com/relex/practice/bean/UserBean.java
@@ -7,7 +7,12 @@
 package com.relex.practice.bean;
 
 import com.relex.practice.pojo.User;
+import java.io.Serializable;
 import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,21 +21,67 @@ import javax.persistence.Persistence;
  *
  * @author alex
  */
-public class UserBean 
+
+@ManagedBean
+public class UserBean implements Serializable  
 {
-    public void addUser(String name, String surname,String email,Date date)
+    private String name;
+    private  String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+   private String email;
+  private  Date date;
+    public void addUser()
     {
         User usr= new User();
         usr.setName(name);
         usr.setSurname(surname);
         usr.setMail(email);
         usr.setData(date);
-        usr.setDivision(null);
+        //usr.setDivision(null);
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+            Class.forName("org.postgresql.Driver");
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("manager");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         em.persist(usr);
         em.getTransaction().commit();
         em.close();
+         FacesContext.getCurrentInstance().addMessage("OK!", new FacesMessage("AHA"));
     }
 }
